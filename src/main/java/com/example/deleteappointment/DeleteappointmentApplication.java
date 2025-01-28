1e9d153a7bbea763ef250043a56787fa962dc786
DavidSebas20 <davidsebastian20@outlook.com>
2025-01-27 20:49:05 -0500

    :tada: initialize deleteappointment project with Spring Boot and Maven setup

---
+++ src/main/java/com/example/deleteappointment/DeleteappointmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.deleteappointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeleteappointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeleteappointmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deleteappointment/DeleteappointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deleteappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeleteappointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
